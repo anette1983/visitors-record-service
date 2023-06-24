d5516df92a966184338bd0cf8d294df68a898fe7
Hanna <anna_kon@bigmir.net>
2023-06-25 00:22:31 +0300

    config update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vite-bundle/",
+  base: "/visitors-record-service/",
   plugins: [react()],
   server: {
     port: 8080,
